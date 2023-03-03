64ea469fe7b794cde805a7423ff84f9a0aa1b500
mikeolams <mikeolams@yahoo.com>
2023-03-03 22:04:12 +0100

    update product route

--- src/handlers/productsRoute.ts
+++ src/handlers/productsRoute.ts
@@ -1,12 +1,12 @@
 import express, {Request, Response} from 'express'
 import {Product, ProductStore} from '../models/product'
-import cors from 'cors'
+// import cors from 'cors'
 
 
-const corsOptions = {
-	origin:'http://someotherdomain.com',
-	optionsSuccessStatus: 200 //some legacy browsers (IE11,various)
-}
+// const corsOptions = {
+// 	origin:'http://someotherdomain.com',
+// 	optionsSuccessStatus: 200 //some legacy browsers (IE11,various)
+// }
 
 
 const store = new ProductStore()
