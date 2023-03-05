66fa738acaa9b17b8b31dd5bb847cd7d21b41ffe
mikeolams <mikeolams@yahoo.com>
2023-03-05 21:30:06 +0100

    updating model tests suite

---
+++ src/models/tests/order_spec.ts
@@ -0,0 +1,87 @@
+import { Order, OrderStore } from '../order';
+
+ 
+const store = new OrderStore()
+// const User = User
+const id = 1
+
+
+describe("Oder Model", () => {
+  it('should have an index method', () => {
+    expect(store.index).toBeDefined();
+  });
+
+  it('should have a show method', () => {
+    expect(store.completeUserOrders).toBeDefined();
+  });
+
+  it('should have a create method', () => {
+    expect(store.createOrder).toBeDefined();
+  });
+
+  it('should have a create method', () => {
+    expect(store.addOrder).toBeDefined();
+  });
+
+
+it('create method should create an Order', async () => {
+    const result = await store.createOrder(
+      {
+        id: 1,
+        productId: 4,
+        userId: 2,
+        productQuantityOrder: 3,
+        orderStatus: 'active'
+
+      }
+  )
+    expect(result).toEqual({
+        id: 1,
+        productId: 4,
+        userId: 2,
+        productQuantityOrder: 3,
+        orderStatus: 'active'
+    });
+  });
+
+  it('index method should return a list of product orders', async () => {
+    const result = await store.index(id);
+    expect(result).toEqual([{
+        id: 1,
+        productId: 4,
+        userId: 2,
+        productQuantityOrder: 3,
+        orderStatus: 'active'
+    }]);
+  });
+
+  it('show method should return the correct order  with the id ', async () => {
+    const result = await store.completeUserOrders(id)
+    expect(result).toEqual({
+        id: 1,
+        productId: 4,
+        userId: 2,
+        productQuantityOrder: 3,
+        orderStatus: 'active'
+    });
+  });
+
+  
+it('create method should create an Order', async () => {
+    const result = await store.addOrder(
+         1,
+         4,
+        2,
+        3,
+        'active'  
+  )
+    expect(result).toEqual({
+        id: 1,
+        productId: 4,
+        userId: 2,
+        productQuantityOrder: 3,
+        orderStatus: 'active'
+    });
+  });
+
+});
--- src/models/tests/product_spec.ts
+++ src/models/tests/product_spec.ts
@@ -1,7 +1,9 @@
 import { Product, ProductStore } from '../product';
+// import {Request, Response, NextFunction} from 'express'
 
  
 const store = new ProductStore()
+const id = 1
 
 
 describe("Product Model", () => {
@@ -18,14 +20,21 @@ describe("Product Model", () => {
   });
 
   it('should have a show category method', () => {
+    expect(store.showCategory).toBeDefined();
+  });
+
+  it('should have a show category method', () => {
     expect(store.showTopFive).toBeDefined();
   });
 
 it('create method should add a Product', async () => {
     const result = await store.addProduct(
-        'Terabithia',
-        250,
-        'active'
+      {
+        id: 1,
+        productName: 'Terabithia',
+        price: 250,
+        category: 'active'
+      }
   )
     expect(result).toEqual({
       id: 1,
@@ -45,14 +54,39 @@ it('create method should add a Product', async () => {
     }]);
   });
 
-  it('show method should return the correct product category', async () => {
-    const result = await store.show('active');
+  it('show method should return the correct product  with the id ', async () => {
+    const result = await store.show(id)
     expect(result).toEqual({
       id:1,
       productName: 'Terabithia',
       price: 250,
       category: 'active'
     });
+  });
+
+  it('showCategory method should return the correct product category', async () => {
+    const result = await store.showCategory('active')
+    expect(result).toEqual({
+      id:1,
+      productName: 'Terabithia',
+      price: 250,
+      category: 'active'
+    });
+  });
+
+  it('showTopFive method should return the top five products', async () => {
+    const result = await store.showTopFive()
+    expect(result).toBe({
+      id:1,
+      productName: 'Terabithia',
+      price: 250,
+      category: 'active'
+    },
+    {id:2,
+    productName: 'Tilapia',
+    price: 200,
+    category: 'complete'
+  });
   });
 
 });
